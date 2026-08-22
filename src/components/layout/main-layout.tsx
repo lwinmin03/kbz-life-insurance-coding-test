4e1ddd6ca57d742ba2ca05160b57edb1dfa6186f
Lwin Min Thein <lwinthein679@gmail.com>
2026-08-22 12:01:01 +0630

    add:(scroll)

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -7,15 +7,13 @@ export default function MainLayout() {
   const isMainProductPage = location.pathname === "/";
 
   return (
-    <div className="h-dvh w-full flex flex-col bg-white overflow-hidden">
+    <div className="min-h-screen w-full flex flex-col bg-white relative overflow-x-hidden">
       <Navbar />
       {isMainProductPage && (
-        <div className="absolute z-0 right-0 top-4/8 -translate-y-1/2 w-110 h-62.5 pointer-events-none bg-linear-to-l from-[#4B8785]/20 to-[#4B8785]/0" />
+        <div className="absolute z-0 right-0 top-1/2 -translate-y-1/2 w-110 h-62.5 pointer-events-none bg-linear-to-l from-[#4B8785]/20 to-[#4B8785]/0" />
       )}
-      <main className="flex-1 min-h-0 w-[92%] mx-auto flex">
-        <div className="w-full h-full flex flex-col">
-          <Outlet />
-        </div>
+      <main className="flex-1 w-[92%] mx-auto flex flex-col">
+        <Outlet />
       </main>
     </div>
   );
